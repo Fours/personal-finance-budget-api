410b0cd49f9e322598af23435e8c836349b15fef
Kyle Willett <kylewillett@yahoo.com>
2026-05-04 14:18:47 -0500

    Added express app with not found route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world!")
+import express from "express"
+import type { Express, Request, Response } from "express"
+import cors from "cors"
+
+type Message = {
+    message: string
+}
+
+const PORT = 8000
+const app: Express = express()
+
+app.use(cors())
+
+app.use((req: Request, res: Response<Message>): void => {
+    res.status(404).json({message: "No endpoint found"})
+})
+
+const server = app.listen(PORT, (): void => {
+    console.log(`Listening on port: ${PORT}`)
+})
+
+// module.exports = server; // for tests
