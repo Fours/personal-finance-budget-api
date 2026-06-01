646ca370fda2ed6467f581c4751a875dafd84a21
Kyle Willett <kylewillett@yahoo.com>
2026-06-01 14:01:05 -0500

    Finished transaction repo tests

--- src/repositories/TransactionRepository.test.ts
+++ src/repositories/TransactionRepository.test.ts
@@ -127,20 +127,106 @@ describe("TransactionRepository", () => {
         })
 
         it("should update a transaction correctly", async () => {
-            
+            const transaction = new Transaction("id", "userId", "date", "merchant", "note", 1, "ctgId", "account", "kind")
+            const spyUpdate = jest.spyOn(prisma.transaction, "update").mockResolvedValue(transaction)
+            const result = await transactionRepo.update("id", "userId", "date", "merchant", "note", 1, "ctgId", "account", "kind")
+            expect(result).toBe(transaction)
+            expect(spyUpdate.mock.calls.length).toBe(1)
+            expect(spyUpdate.mock.calls[0]).toEqual([{
+                where: { id: "id", userId: "userId" },
+                data: {
+                    date: "date",
+                    merchant: "merchant",
+                    note: "note",
+                    amount: 1,
+                    categoryId: "ctgId",
+                    account: "account",
+                    kind: "kind"
+                },
+            }])
         })
 
-        // success with values
-
-        // success with undefined
-        
+        it("when params are undefined should update a transaction correctly", async () => {
+            const transaction = new Transaction("id", "userId", "date", "merchant", "note", 1, "ctgId", "account", "kind")
+            const spyUpdate = jest.spyOn(prisma.transaction, "update").mockResolvedValue(transaction)
+            const result = await transactionRepo.update("id", "userId")
+            expect(result).toBe(transaction)
+            expect(spyUpdate.mock.calls.length).toBe(1)
+            expect(spyUpdate.mock.calls[0]).toEqual([{
+                where: { id: "id", userId: "userId" },
+                data: {}
+            }])
+        })
     })
 
     describe("getAll", () => {
+
+        it("should get transactions correctly", async () => {
+            const transaction = new Transaction("id", "userId", "date", "merchant", "note", 1, "ctgId", "account", "kind")
+            const spyFind = jest.spyOn(prisma.transaction, "findMany").mockResolvedValue([transaction])
+            const result = await transactionRepo.getAll("userId", 10, 20)
+            expect(result).toEqual([transaction])
+            expect(spyFind.mock.calls.length).toBe(1)
+            expect(spyFind.mock.calls[0]).toEqual([{
+                where: { userId: "userId" },
+                take: 10,
+                skip: 20,
+                orderBy: { date: 'desc' }
+            }])
+        })
         
+        it("when params are undefined should use defaults and get transactions correctly", async () => {
+            const transaction = new Transaction("id", "userId", "date", "merchant", "note", 1, "ctgId", "account", "kind")
+            const spyFind = jest.spyOn(prisma.transaction, "findMany").mockResolvedValue([transaction])
+            const result = await transactionRepo.getAll("userId")
+            expect(result).toEqual([transaction])
+            expect(spyFind.mock.calls.length).toBe(1)
+            expect(spyFind.mock.calls[0]).toEqual([{
+                where: { userId: "userId" },
+                take: DB_DEFAULT_LIMIT,
+                skip: 0,
+                orderBy: { date: 'desc' }
+            }])
+        })
     })
 
     describe("delete", () => {
-        
+
+        it("When a PrismaClientKnownRequestError with error code P2025 occurs, transform it to NotFound", async () => {
+            const dbError = new PrismaClientKnownRequestError("test", {code: "P2025", clientVersion: ""})
+            jest.spyOn(prisma.transaction, "delete").mockRejectedValue(dbError)
+            try {
+                await transactionRepo.delete("id", "userId")
+                throw new Error("test failed")
+            } catch(error) {
+                expect(error instanceof NotFound).toBe(true)
+                if (error instanceof NotFound) {
+                    expect(error.message).toBe("Transaction was not found")
+                }
+            }
+        })
+
+        it("when a different db error occurs should fail with that error", async () => {
+            const dbError = new Error("different db error")
+            jest.spyOn(prisma.transaction, "delete").mockRejectedValue(dbError)
+            try {
+                await transactionRepo.delete("id", "userId")
+                throw new Error("test failed")
+            } catch(error) {
+                expect(error instanceof Error).toBe(true)
+                if (error instanceof Error) {
+                    expect(error.message).toBe("different db error")
+                }
+            }
+        })
+
+        it("should delete a transaction correctly", async () => {
+            const spyDelete = jest.spyOn(prisma.transaction, "delete").mockResolvedValue(undefined)
+            await transactionRepo.delete("id", "userId")
+            expect(spyDelete.mock.calls.length).toBe(1)
+            expect(spyDelete.mock.calls[0]).toEqual([{
+                where: { id: "id", userId: "userId" }
+            }])
+        })
     })
 })
